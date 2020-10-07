a938ba06d1c9b356603c7cdfd756f08b10ca64a7
div-antk <51283468+owaii@users.noreply.github.com>
2020-10-07 22:51:57 +0900

    ドロップダウンを横に表示

---
+++ js/js.js
@@ -0,0 +1,23 @@
+document.getElementById("menu1").addEventListener("mouseover", function() {
+  document.getElementById("dropmenu1").style.display = "block";
+}, false);
+
+document.getElementById("menu1").addEventListener("mouseout", function() {
+  document.getElementById("dropmenu1").style.display = "none";
+}, false);
+
+document.getElementById("menu2").addEventListener("mouseover", function() {
+  document.getElementById("dropmenu2").style.display = "block";
+}, false);
+
+document.getElementById("menu2").addEventListener("mouseout", function() {
+  document.getElementById("dropmenu2").style.display = "none";
+}, false);
+
+document.getElementById("menu3").addEventListener("mouseover", function() {
+  document.getElementById("dropmenu3").style.display = "block";
+}, false);
+
+document.getElementById("menu3").addEventListener("mouseout", function() {
+  document.getElementById("dropmenu3").style.display = "none";
+}, false);
