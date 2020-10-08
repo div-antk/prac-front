a407bba8948877bf1853dd4e9c6ff0c7832d498f
div-antk <51283468+owaii@users.noreply.github.com>
2020-10-08 22:17:25 +0900

    グローバルメニューの骨子

--- js/js.js
+++ js/js.js
@@ -1,23 +1,23 @@
-document.getElementById("menu1").addEventListener("mouseover", function() {
-  document.getElementById("dropmenu1").style.display = "block";
+document.getElementById("side_menu1").addEventListener("mouseover", function() {
+  document.getElementById("dropside_menu1").style.display = "block";
 }, false);
 
-document.getElementById("menu1").addEventListener("mouseout", function() {
-  document.getElementById("dropmenu1").style.display = "none";
+document.getElementById("side_menu1").addEventListener("mouseout", function() {
+  document.getElementById("dropside_menu1").style.display = "none";
 }, false);
 
-document.getElementById("menu2").addEventListener("mouseover", function() {
-  document.getElementById("dropmenu2").style.display = "block";
+document.getElementById("side_menu2").addEventListener("mouseover", function() {
+  document.getElementById("dropside_menu2").style.display = "block";
 }, false);
 
-document.getElementById("menu2").addEventListener("mouseout", function() {
-  document.getElementById("dropmenu2").style.display = "none";
+document.getElementById("side_menu2").addEventListener("mouseout", function() {
+  document.getElementById("dropside_menu2").style.display = "none";
 }, false);
 
-document.getElementById("menu3").addEventListener("mouseover", function() {
-  document.getElementById("dropmenu3").style.display = "block";
+document.getElementById("side_menu3").addEventListener("mouseover", function() {
+  document.getElementById("dropside_menu3").style.display = "block";
 }, false);
 
-document.getElementById("menu3").addEventListener("mouseout", function() {
-  document.getElementById("dropmenu3").style.display = "none";
+document.getElementById("side_menu3").addEventListener("mouseout", function() {
+  document.getElementById("dropside_menu3").style.display = "none";
 }, false);
