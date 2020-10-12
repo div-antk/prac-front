48d2528dd93ab585070a22de32193f5c45bc62fb
div-antk <51283468+owaii@users.noreply.github.com>
2020-10-12 23:03:07 +0900

    swiper実装

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  autoplay: true,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  },
+  });
