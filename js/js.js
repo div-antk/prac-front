744c686b37d5b2e2816c30bdf4c59d129200c6b8
div-antk <51283468+owaii@users.noreply.github.com>
2020-10-15 23:46:59 +0900

    マウスオーバー処理途中

--- js/js.js
+++ js/js.js
@@ -6,13 +6,11 @@ document.getElementById("side_menu1").addEventListener("mouseover", function() {
 document.getElementById("side_menu1").addEventListener("mouseout", function() {
   document.getElementById("dropside_menu1").style.display = "none";
   document.getElementById("side_menu1").style.background = "none";
-
 }, false);
 
 document.getElementById("side_menu2").addEventListener("mouseover", function() {
   document.getElementById("dropside_menu2").style.display = "block";
   document.getElementById("side_menu2").style.background = "#f01133";
-
 }, false);
 
 document.getElementById("side_menu2").addEventListener("mouseout", function() {
@@ -37,11 +35,3 @@ document.getElementById("side_menu4").addEventListener("mouseover", function() {
 document.getElementById("side_menu4").addEventListener("mouseout", function() {
   document.getElementById("dropside_menu4").style.display = "none";
 }, false);
-
-document.querySelector("li.gnavi_menu1").addEventListener("mouseenter", function() {
-  console.log('うどん');
-});
-
-document.querySelector("li.gnavi_menu2").addEventListener("mouseenter", function() {
-  console.log('おでん');
-});
--- js/swiper.js
+++ js/swiper.js
@@ -9,4 +9,14 @@ var swiper = new Swiper('.swiper-container', {
   nextEl: '.swiper-button-next',
   prevEl: '.swiper-button-prev',
   },
-  });
+});
+
+document.getElementsByClassName("swiper-container").addEventListener("mouseover", function() {
+  console.log("(´・ω・｀)");
+  document.getElementsByClassName("swiper-button-prev").style.display = "block";
+}, false);
+
+// document.getElementById("swiper-container").addEventListener("mouseout", function() {
+//   document.getElementById("dropside_menu1").style.display = "none";
+//   document.getElementById("side_menu1").style.background = "none";
+// }, false);
